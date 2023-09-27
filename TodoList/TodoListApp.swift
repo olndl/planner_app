7e26e82fb8e0745ebcd1918960a394e6515ccff9
Olga Shiryavceva <olga@newagesports.net>
2023-09-27 23:19:27 +0300

    Initial Commit

---
+++ TodoList/TodoListApp.swift
@@ -0,0 +1,17 @@
+//
+//  TodoListApp.swift
+//  TodoList
+//
+//  Created by Ольга Ширявцева on 27.09.2023.
+//
+
+import SwiftUI
+
+@main
+struct TodoListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
