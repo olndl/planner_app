27f406aab5c56e9fc9b689b213057e80527c0fe9
Olga Shiryavceva <olga@newagesports.net>
2023-09-29 01:10:06 +0300

    initial setup

---
+++ TodoList/View/NewTodoView.swift
@@ -0,0 +1,20 @@
+//
+//  NewTodoView.swift
+//  TodoList
+//
+//  Created by Ольга Ширявцева on 29.09.2023.
+//
+
+import SwiftUI
+
+struct NewTodoView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct NewTodoView_Previews: PreviewProvider {
+    static var previews: some View {
+        NewTodoView()
+    }
+}
---
+++ TodoList/ViewModel/MainViewViewModel.swift
@@ -0,0 +1,8 @@
+//
+//  MainViewViewModel.swift
+//  TodoList
+//
+//  Created by Ольга Ширявцева on 29.09.2023.
+//
+
+import Foundation
