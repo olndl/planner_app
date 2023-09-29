20a14b521ab7d02b5c9f47f2e763202767d1cfc5
Olga Shiryavceva <olga@newagesports.net>
2023-09-29 22:28:03 +0300

    feat: adds header as individual component

--- TodoList/View/HeaderView.swift
+++ TodoList/View/HeaderView.swift
@@ -1,33 +1,13 @@
-//
-//  HeaderView.swift
-//  TodoList
-//
-//  Created by Ольга Ширявцева on 29.09.2023.
-//
-
 import SwiftUI
 
 
 struct HeaderView: View {
-    
-    let colors: [Color] =
-       [.red, .orange, .purple, .green, .yellow, .blue,]
 
     
     var body: some View {
         ZStack {
-            RoundedRectangle(cornerRadius: 0)
-                .foregroundColor(Color.cyan)
-                .rotationEffect(Angle(degrees: -10))
-            HStack {
-                ForEach(colors.indices, id: \.self) {
-                    Rectangle()
-                        .fill(colors[$0])
-                        .frame(width: 100, height: 100)
-                        .offset(x: CGFloat($0) * 15.0,
-                                y: CGFloat($0) * 15.0)
-                }
-            }.padding([.bottom, .top], 160)
+            RoundedRectangle(cornerRadius: 12)
+                .foregroundColor(Color.teal)
             VStack{
                 Text("My Planner")
                     .foregroundColor(Color.white)
@@ -36,11 +16,11 @@ struct HeaderView: View {
                 Text("Get Things Done")
                     .foregroundColor(Color.white)
                     .font(.system(size: 30))
-            }.padding(.top, 280)
+            }.padding(.top, 15)
             
         }
-        .frame(width: UIScreen.main.bounds.width * 3, height: 300)
-        .offset(y: -100)
+        .frame(width: UIScreen.main.bounds.width - 20, height: 250)
+        .offset(y: -30)
         
     }
 }
