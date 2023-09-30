1a4fcfcc4a3d0cbda02763b734a53a2db0c959cb
Olga Shiryavceva <olga@newagesports.net>
2023-09-30 20:22:17 +0300

    feat: adds sign up view

--- TodoList/View/HeaderView.swift
+++ TodoList/View/HeaderView.swift
@@ -2,31 +2,36 @@ import SwiftUI
 
 
 struct HeaderView: View {
-
+    let title: String
+    let subtitle: String
+    let backgroundColor: Color
+    let cornerRadius: Double
+    let angle: Double
     
     var body: some View {
         ZStack {
-            RoundedRectangle(cornerRadius: 12)
-                .foregroundColor(Color.teal)
+            RoundedRectangle(cornerRadius: cornerRadius)
+                .rotationEffect(Angle(degrees: angle))
+                .foregroundColor(backgroundColor)
             VStack{
-                Text("My Planner")
+                Text(title)
                     .foregroundColor(Color.white)
                     .font(.system(size: 50))
                     .bold()
-                Text("Get Things Done")
+                Text(subtitle)
                     .foregroundColor(Color.white)
                     .font(.system(size: 30))
-            }.padding(.top, 15)
+            }.padding(.top, 130)
             
         }
-        .frame(width: UIScreen.main.bounds.width - 20, height: 250)
-        .offset(y: -30)
+        .frame(width: UIScreen.main.bounds.width * 3, height: 350)
+        .offset(y: -200)
         
     }
 }
 
 struct HeaderView_Previews: PreviewProvider {
     static var previews: some View {
-        HeaderView()
+        HeaderView(title: "Title", subtitle: "Subtitle", backgroundColor: .blue, cornerRadius: 0, angle: -15)
     }
 }
--- TodoList/View/SignUpView.swift
+++ TodoList/View/SignUpView.swift
@@ -8,8 +8,32 @@
 import SwiftUI
 
 struct SignUpView: View {
+    @State var name = ""
+    @State var email = ""
+    @State var password = ""
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack{
+            HeaderView(title: "Sign Up", subtitle: "Start organizing your plans", backgroundColor: Color.pink, cornerRadius: 0, angle: 15)
+            Form{
+                TextField("Full Name", text: $name)
+                    .textFieldStyle(DefaultTextFieldStyle())
+                    .autocorrectionDisabled()
+                TextField("Email", text: $email).textFieldStyle(DefaultTextFieldStyle())
+                    .autocapitalization(.none)
+                    .autocorrectionDisabled()
+                SecureField("Password", text: $password)
+                    .textFieldStyle(DefaultTextFieldStyle())
+                CustomButtonView(title: "Create account", backgroundColor: .green, action: {
+                    //sign up
+                })
+            
+            }
+            .cornerRadius(12)
+            .frame(width: UIScreen.main.bounds.width - 20, height: 280)
+            .offset(y: -100)
+        
+       
+        }
     }
 }
 
